21860b05c3320239a04195452a1979bf7fd7e72f
gaurav4332 <gaurav4332@gmail.com>
2024-02-07 15:40:04 +0530

    file read

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const http = require("http");
-
-http.createServer((req, resp) => {
-    resp.write("<h1>Baba Rock bdhdbhvb </h1>");
-    resp.end();
-  })
-  .listen(1601);
+const fs= require('fs')
+fs.readFile('./file/a.txt','utf-8',(err,data)=>{
+  if(err) throw err
+  console.log(data)
+})
